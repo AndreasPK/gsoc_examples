9d916a91a44057b17a8077d021a8d6bcd65c0f1e
Andreas Klebinger <klebinger.andreas@gmx.at>
2018-03-26 20:36:31 +0200

    Gen SH updated

--- gen.sh
+++ gen.sh
@@ -3,20 +3,21 @@
 if [ -u {$1+""} ]; then
     exit
 fi
-
 echo "Compiling '$1'"
-GHC=$2
 
+GHC=$2
 if [ -z ${GHC} ]; then
     GHC="stack --resolver=nightly exec -- ghc"
 fi
+echo "Using ghc command: '$GHC'"
 
-echo "Using '$GHC'"
+TIME=20
+echo "Running benchmarks for at least $TIME seconds."
 
 $GHC -O2 -ddump-cmm -ddump-simpl -ddump-asm -ddump-to-file -dsuppress-all "$1.s" -package criterion -package random -o "$1"
 $GHC -O2 -ddump-cmm -ddump-simpl -ddump-asm -ddump-to-file -dsuppress-all "$1_mod.s" -package criterion -package random -o "$1_mod"
 
 echo "Benchmarking unmodified version"
-./$1.exe -L30 -I 0.95 -v2
+./$1.exe -L $TIME -I 0.95 -v2
 echo "Benchmarking modified version"
-./$1_mod.exe -I 0.95 -L30 -v2
+./$1_mod.exe -I 0.95 -L $TIME -v2
